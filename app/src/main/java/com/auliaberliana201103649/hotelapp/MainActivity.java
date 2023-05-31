568bc93166ba2ae176330ae1a31c79179b28fd2d
201103649 Aulia Berliana <auliaaberliana@gmail.com>
2023-05-31 14:04:02 +0700

    hotel

---
+++ app/src/main/java/com/auliaberliana201103649/hotelapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.auliaberliana201103649.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        String imageUrl = "https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdGVsfGVufDB8fDB8fHww&w=1000&q=80";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+
+
+    }
+}
